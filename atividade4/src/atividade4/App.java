30988edb0fe6b114ec193a30b6c0813e2c934c60
José Itauan Araujo da Rocha <20211064010011@ifrn.local>
2024-12-10 14:11:32 -0300

    Projeto Pronto!

---
+++ atividade4/src/atividade4/App.java
@@ -0,0 +1,15 @@
+package atividade4;
+
+public class App {
+
+	public static void main(String[] args) {
+        // Criando uma instância de Pessoa para cada estuantes
+        
+        Pessoa pessoa2 = new Pessoa("Jose Itauan Araujo da Rocha", 19);
+        
+        // Exibindo as informações dos estudantes
+        
+        pessoa2.exibirInformacoes();
+    }
+
+}
